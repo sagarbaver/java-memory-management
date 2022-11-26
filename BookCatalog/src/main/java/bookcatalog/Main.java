7b7e72910020d1b07d9ca7263e14fffecf8b07dc
Sagar Baver <sagar.baver@autodesk.com>
2022-11-26 11:36:49 -0800

    (chore): Adds exercise files from Java memeory management course

---
+++ BookCatalog/src/main/java/bookcatalog/Main.java
@@ -0,0 +1,16 @@
+package bookcatalog;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BookCollection bc = new BookCollection();
+
+		bc.printAllBooks();
+		//get price of book called Tom Jones in EUR
+		System.out.println(bc.findBookByName("Tom Jones").getPrice().convert("EUR"));
+		bc.printAllBooks(); // Notice that the price of Tom Jones has changed.
+		
+	}
+}
---
+++ EscapingReferences/src/main/java/escaperef/Customer.java
@@ -0,0 +1,18 @@
+package escaperef;
+
+public class Customer {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+		public String toString() {
+		return name;
+	}
+	
+}
