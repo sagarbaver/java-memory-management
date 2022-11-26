14d4f35263ac3494dc016b72f7a7334d476d4b0a
Sagar Baver <sagar.baver@autodesk.com>
2022-11-26 11:39:34 -0800

    (chore): Adds exercise files from Java memeory management course (#1)

---
+++ BookCatalog/src/main/java/bookcatalog/Price.java
@@ -0,0 +1,41 @@
+package bookcatalog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Price {
+
+	private Map<String,Double> rates;
+	private Double value;
+	
+	public Price(Double value) {
+		this.value = value;
+		rates = new HashMap<String,Double>();
+		rates.put("USD", 1d);
+		rates.put("GBP", 0.6);
+		rates.put("EUR", 0.8);
+	}
+		
+	public Double convert(String toCurrency) {
+		
+		if (toCurrency.equals("USD")) {
+			return value; // escaping reference
+			// return new Double(value); -- Good
+		}
+		else {
+			Double conversion = rates.get("USD") / rates.get(toCurrency);
+			value = conversion * value; // mutation
+			return value;
+			// return conversion * value; -- Good
+		}
+	}
+	
+	public String toString() {
+		return this.value.toString();
+	}
+	
+	public Map<String,Double> getRates() { // Do not expose this method to make Price immutable.
+		return rates; // escaping reference
+	}
+	
+}
