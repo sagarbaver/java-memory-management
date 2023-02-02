d7338c6f4addac68718d3ce75760cacae2e51ad7
Sagar Baver <sagar.baver@autodesk.com>
2023-02-01 22:00:34 -0800

    (chore): Updates GC module code

--- GarbageCollection/src/main/java/gc/Customer.java
+++ GarbageCollection/src/main/java/gc/Customer.java
@@ -1,27 +1,32 @@
 package gc;
 
-public class Customer  {
-	private String name;
+public class Customer {
 
-	public String getName() {
-		return name;
-	}
-	
-	public Customer(String name) {
-		this.name = name;
-	}
-	
-	public Customer(Customer oldCustomer) {
-		this.name = oldCustomer.name; 
-	}
-	
-	
-	public String toString() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public Customer(String name) {
+    this.name = name;
+  }
+
+  public Customer(Customer oldCustomer) {
+    this.name = oldCustomer.name;
+  }
+
+
+  public String toString() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void finalize() {
+    System.out.println("The object was GC'ed.");
+  }
+
 }
