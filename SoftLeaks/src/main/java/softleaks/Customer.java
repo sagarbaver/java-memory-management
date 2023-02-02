57bd0d865806259866366b57d1dfb3af52174b1d
Sagar Baver <sagar.baver@autodesk.com>
2023-02-01 22:26:05 -0800

    (chore): Updates SoftLeaks module code

--- SoftLeaks/src/main/java/softleaks/Customer.java
+++ SoftLeaks/src/main/java/softleaks/Customer.java
@@ -2,22 +2,22 @@ package softleaks;
 
 public class Customer {
 
-	private int id;
-	private String name;
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String toString() {
-		return id + " : " + name;
-	}
-	
-	public Customer(String name) {
-		super();
-		this.name = name;
-	}
+  private int id;
+  private String name;
 
-	
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String toString() {
+    return id + " : " + name;
+  }
+
+  public Customer(String name) {
+    super();
+    this.name = name;
+  }
+
+
 }
 
