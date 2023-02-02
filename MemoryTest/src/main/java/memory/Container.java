ffc07a8def0594887ea91fc581f2552fd16b364b
Sagar Baver <sagar.baver@autodesk.com>
2023-02-01 19:59:45 -0800

    (chore): Reformats files under MemoryTest

--- MemoryTest/src/main/java/memory/Container.java
+++ MemoryTest/src/main/java/memory/Container.java
@@ -1,13 +1,14 @@
 package memory;
 
 public class Container {
-	private String initial = "A";
-	
-	public String getInitial() {
-		return initial;
-	}
-	
-	public void setInitial(String initial) {
-		this.initial = initial;
-	}
+
+  private String initial = "A";
+
+  public String getInitial() {
+    return initial;
+  }
+
+  public void setInitial(String initial) {
+    this.initial = initial;
+  }
 }
